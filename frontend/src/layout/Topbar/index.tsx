9feff6fc5c24e7ba4b07be60c48865af661119ad
Pradyut Das <77787607+prxdyut@users.noreply.github.com>
2024-07-30 15:34:42 +0530

    maino

--- frontend/src/layout/Topbar/index.tsx
+++ frontend/src/layout/Topbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { PopupButton } from "../../components/PopupButton";
 import { NotifyAbsentee } from "../../components/NotifyAbsentee";
